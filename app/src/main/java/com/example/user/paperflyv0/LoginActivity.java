07d8b49852c62ae3703ddc1d02ece3985b0ee19f
alavitabassum <alavitabassum@gmail.com>
2018-07-25 14:02:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/paperflyv0/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.paperflyv0;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button orderlist;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        orderlist = findViewById(R.id.sign_in);
+
+
+        // Capture button clicks
+        orderlist.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(LoginActivity.this,
+                        PickUpActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/user/paperflyv0/otp.java
@@ -0,0 +1,13 @@
+package com.example.user.paperflyv0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
