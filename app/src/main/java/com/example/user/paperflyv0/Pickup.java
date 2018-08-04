1c2819794d6208cbe3d857437ce54be35779100e
fatimamirza94 <38617724+fatimamirza94@users.noreply.github.com>
2018-08-04 20:27:34 +0600

    read database commit

---
+++ app/src/main/java/com/example/user/paperflyv0/Pickup.java
@@ -0,0 +1,34 @@
+package com.example.user.paperflyv0;
+
+import java.io.Serializable;
+
+public class Pickup {
+    private int id;
+    private String merchantName, merchantAddress, scheduleTime;
+
+    public Pickup(int id, String merchantName, String merchantAddress, String scheduleTime) {
+        this.id = id;
+        this.merchantName = merchantName;
+        this.merchantAddress = merchantAddress;
+        this.scheduleTime = scheduleTime;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMerchantName() {
+        return merchantName;
+    }
+
+    public String getMerchantAddress() {
+        return merchantAddress;
+    }
+
+
+
+    public String getScheduleTime() {
+        return scheduleTime;
+    }
+}
