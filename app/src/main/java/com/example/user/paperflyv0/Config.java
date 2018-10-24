7b0385145638e9c12fda19b77f62e0b2e49b16fa
nawshin603 <nawshin603@gmail.com>
2018-10-24 11:28:41 +0600

    Login API Integration

---
+++ app/src/main/java/com/example/user/paperflyv0/Config.java
@@ -0,0 +1,15 @@
+package com.example.user.paperflyv0;
+
+public class Config {
+
+    //Keys for Sharedpreferences
+    //This would be the name of our shared preferences
+    public static final String SHARED_PREF_NAME = "myloginapp";
+
+    //This would be used to store the email of current logged in user
+    public static final String EMAIL_SHARED_PREF = "username";
+
+    //We will use this to store the boolean in sharedpreference to track user is loggedin or not
+    public static final String LOGGEDIN_SHARED_PREF = "loggedin";
+
+}
--- app/src/main/java/com/example/user/paperflyv0/Welcome2.java
+++ app/src/main/java/com/example/user/paperflyv0/Welcome2.java
@@ -4,33 +4,28 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Welcome extends AppCompatActivity {
-
+public class Welcome2 extends AppCompatActivity {
 
     Button signout;
     TextView welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-        welcome = (TextView) findViewById(R.id.textView);
+        setContentView(R.layout.activity_welcome2);
 
         //Fetching email from shared preferences
         SharedPreferences sharedPreferences = getSharedPreferences(Config.SHARED_PREF_NAME, Context.MODE_PRIVATE);
         String username = sharedPreferences.getString(Config.EMAIL_SHARED_PREF,"Not Available");
 
         //Showing the current logged in email to textview
-        welcome.setText("Current User: " + username );
+        //welcome.setText("Current User: " + username );
 
 
         signout = findViewById(R.id.sign_out);
@@ -40,8 +35,8 @@ public class Welcome extends AppCompatActivity {
                 logout();
             }
         });
-
     }
+
 
     //Logout function
     private void logout(){
@@ -68,7 +63,7 @@ public class Welcome extends AppCompatActivity {
                         editor.commit();
 
                         //Starting login activity
-                        Intent intent = new Intent(Welcome.this, LoginActivity.class);
+                        Intent intent = new Intent(Welcome2.this, LoginActivity.class);
                         startActivity(intent);
                     }
                 });
