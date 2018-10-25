90b35299513e25a420e6a508ef1bf92dc103d43a
alavitabassum <alavitabassum@gmail.com>
2018-10-25 12:37:18 +0600

    modified

--- app/src/main/java/com/example/user/paperflyv0/TodaySummary.java
+++ app/src/main/java/com/example/user/paperflyv0/TodaySummary.java
@@ -1,4 +1,34 @@
 package com.example.user.paperflyv0;
 
 public class TodaySummary {
+
+    private String m_names;
+    private String asgn_pu;
+    private String upload_pu;
+    private String received_pu;
+
+    public TodaySummary(String m_names,String asgn_pu,String upload_pu,String received_pu)
+    {
+        this.m_names = m_names;
+        this.asgn_pu = asgn_pu;
+        this.upload_pu = upload_pu;
+        this.received_pu = received_pu;
+    }
+
+    public String getM_names()
+    {
+        return m_names;
+    }
+    public String getAsgn_pu()
+    {
+        return asgn_pu;
+    }
+    public String getUpload_pu()
+    {
+        return upload_pu;
+    }
+    public String getReceived_pu()
+    {
+        return received_pu;
+    }
 }
