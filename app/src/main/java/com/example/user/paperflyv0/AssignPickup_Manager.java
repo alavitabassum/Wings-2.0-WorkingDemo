b978b5c9b2a7de483f4638ba1eeabf833d6cd3f7
RidwanaTuli <ridwana.tuli@gmail.com>
2018-12-04 17:56:28 +0600

    merged with nawshin

--- app/src/main/java/com/example/user/paperflyv0/AssignPickup_Manager.java
+++ app/src/main/java/com/example/user/paperflyv0/AssignPickup_Manager.java
@@ -47,7 +47,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
