55b190ddd1029265c5097ff7ea4fa46e052d7913
alavitabassum <alavitabassum@gmail.com>
2019-09-19 11:47:51 +0600

    Dp2 not done data load

--- app/src/main/java/com/paperflywings/user/paperflyv0/DeliveryApp/DeliverySupervisor/DeliverySuperVisorDp2NotDone/DeliverySupDp2NotDoneAdapter.java
+++ app/src/main/java/com/paperflywings/user/paperflyv0/DeliveryApp/DeliverySupervisor/DeliverySuperVisorDp2NotDone/DeliverySupDp2NotDoneAdapter.java
@@ -1,4 +1,167 @@
 package com.paperflywings.user.paperflyv0.DeliveryApp.DeliverySupervisor.DeliverySuperVisorDp2NotDone;
 
-public class DeliverySupDp2NotDoneAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.paperflywings.user.paperflyv0.Databases.BarcodeDbHelper;
+import com.paperflywings.user.paperflyv0.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeliverySupDp2NotDoneAdapter extends RecyclerView.Adapter<DeliverySupDp2NotDoneAdapter.ViewHolder>implements Filterable {
+
+    private List<DeliverySupDp2NotDoneModel> listfull;
+    private List<DeliverySupDp2NotDoneModel> list;
+
+    private int currentPosition = -1;
+    private Context context;
+    BarcodeDbHelper db;
+    private OnItemClickListener mListner;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listner) {
+        this.mListner = listner;
+    }
+
+    public DeliverySupDp2NotDoneAdapter(List<DeliverySupDp2NotDoneModel>list, Context context){
+        this.list = list;
+        this.context = context;
+        listfull = new ArrayList<>(list);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView item_ordId;
+        public TextView item_merOrderRef;
+        public TextView item_merchantName;
+        public TextView item_pickMerchantName;
+        public TextView item_packagePrice;
+        public TextView item_productBrief;
+        public TextView item_sla_miss;
+        public Button item_dp_not_done_button;
+        public CardView card_view;
+
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            item_ordId=itemView.findViewById(R.id.sup_orderId_dp2_not_done);
+            item_merOrderRef=itemView.findViewById(R.id.m_order_ref_dp2_not_done);
+            item_merchantName=itemView.findViewById(R.id.sup_m_name_dp2_not_done);
+            item_packagePrice=itemView.findViewById(R.id.price_dp2_not_done);
+            item_productBrief=itemView.findViewById(R.id.package_brief_dp2_not_done);
+            item_sla_miss = itemView.findViewById(R.id.sla_deliverytime);
+            item_dp_not_done_button = itemView.findViewById(R.id.btn_dp2_not_done);
+
+            item_dp_not_done_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mListner != null) {
+                        // Position of the item will be saved in this variable
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            mListner.onItemClick(itemView, position);
+                        }
+                    }
+                }
+            });
+
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v  = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.delivery_supervisor_dp2_not_done,viewGroup,false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.item_ordId.setText(list.get(i).getOrderid());
+        viewHolder.item_merOrderRef.setText(list.get(i).getMerOrderRef());
+
+        String pickMerchantName = list.get(i).getPickMerchantName();
+        int DeliveryTime = list.get(i).getSlaMiss();
+
+        if(pickMerchantName.equals("")){
+            viewHolder.item_merchantName.setText(list.get(i).getMerchantName());
+        } else if (!pickMerchantName.equals("")) {
+            viewHolder.item_merchantName.setText(list.get(i).getPickMerchantName());
+        }
+
+        if(DeliveryTime<0) {
+            viewHolder.item_sla_miss.setText(String.valueOf(list.get(i).getSlaMiss()));
+            viewHolder.item_sla_miss.setBackgroundResource(R.color.red);
+            viewHolder.item_sla_miss.setTextColor(Color.WHITE);
+        }
+        else if (DeliveryTime>=0){
+            try{
+                viewHolder.item_sla_miss.setText(String.valueOf(list.get(i).getSlaMiss()));
+                viewHolder.item_sla_miss.setBackgroundResource(R.color.green);
+                viewHolder.item_sla_miss.setTextColor(Color.WHITE); }
+            catch (Exception e){
+                Toast.makeText(context, "DeliveryOnholdAdapter "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        viewHolder.item_packagePrice.setText(list.get(i).getPackagePrice()+" Taka");
+        viewHolder.item_productBrief.setText("Product Brief:  "+list.get(i).getProductBrief());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+
+    @Override
+    public Filter getFilter() {
+        return NamesFilter;
+    }
+    private Filter NamesFilter = new Filter() {
+
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<DeliverySupDp2NotDoneModel>filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0){
+                filteredList.addAll(listfull);
+            }else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for(DeliverySupDp2NotDoneModel item: listfull){
+                    if (item.getOrderid().toLowerCase().contains(filterPattern) || item.getMerOrderRef().toLowerCase().contains(filterPattern) || item.getMerchantName().toLowerCase().contains(filterPattern) || item.getPickMerchantName().toLowerCase().contains(filterPattern) || item.getCustname().toLowerCase().contains(filterPattern) || item.getCustphone().toLowerCase().contains(filterPattern)){
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            list.clear();
+            list.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+
+    };
 }
